5972282763a15db75b5f03a2a0466b6279ab9b82
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-29 18:25:59 +0000

    cleaned up styling and added comments

--- numbers.c
+++ numbers.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 
 {
-    float myFloat = 3.87;
-    float other = 1.52;
+    int myFloat = round(3.87 * 100);
+    int other = round(1.52 * 100);
+    int total = myFloat + other;
+    int cents = total % 100;
+    int dollars = total / 100;
+
     float divison = (float)78/(float)15;
 
     // returns the float with the speceifed decimal places
-    printf("adding the numbers: %.2f\n", myFloat + other);
+    printf("adding the numbers: %.2f\n", (float)total / 100);
     printf("the divison: %.2f\n", divison);
 
 }
